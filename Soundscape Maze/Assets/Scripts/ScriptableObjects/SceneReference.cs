0975fd5135b8c590acba3760dfbc7203657a73db
antekgrab <agrabo18@student.aau.dk>
2022-03-29 11:51:14 +0200

    reset

--- Soundscape Maze/Assets/Scripts/ScriptableObjects/SceneReference.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-[CreateAssetMenu(fileName = "Scene Management", menuName = "ScriptableObjects/SceneReference", order = 1)]
-public class SceneReference : ScriptableObject
-{
-    public string Filename;
-}
